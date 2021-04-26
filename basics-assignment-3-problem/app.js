b1a3544ac1022cf9e55a5ed1aa0111515484498c
Kelly <chase.kelly@hp.com>
2021-04-26 09:45:48 -0500

    Assignment 3

---
+++ basics-assignment-3-problem/app.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      results: "Not there yet",
+    };
+  },
+  watch: {
+    result() {
+      const that = this;
+      setTimeout(function () {
+        that.counter = 0;
+      }, 5000);
+    },
+  },
+  computed: {
+    result() {
+      if (this.counter < 37) {
+        return "Not there yet";
+      } else if (this.counter === 37) {
+        return this.counter;
+      } else {
+        return "Too much!";
+      }
+    },
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+  },
+});
+
+app.mount("#assignment");
